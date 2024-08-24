b46a3c012cc07627bd2c6c678fd3d02438de0dab
safabelhouch <safabelhouch@gmail.com>
2024-08-24 15:02:31 +0100

    google analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,22 @@ import { renderRoutes } from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import routes from "./routes";
 
+import ReactGA from 'react-ga'
+
+import usePageViews from "./hooks/usePageViews";
+
+
+const GA_MEASUREMENT_ID = 'G-EEYBFX4JHZ';
+
+ReactGA.initialize(GA_MEASUREMENT_ID);
+
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+    <usePageViews />
     <Analytics />
   </StrictMode>
 );
